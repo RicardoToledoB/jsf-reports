5f57e667c17df8b395cccbaa26909b346f4ed073
ricardotoledo <ricardotoledo@192.168.1.39>
2016-02-09 08:53:04 -0300

    jsf reportes

--- src/main/java/com/proyecto/bean/UsuarioBean.java
+++ src/main/java/com/proyecto/bean/UsuarioBean.java
@@ -12,13 +12,18 @@ import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import net.sf.jasperreports.engine.JRDataSource;
+
 
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+import net.sf.jasperreports.engine.export.ooxml.JRDocxExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRPptxExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
 
 /**
  *
@@ -46,13 +51,11 @@ public class UsuarioBean implements Serializable {
     }
 
     public void init() throws JRException {
-        JRDataSource beanDataSource = new JRBeanCollectionDataSource(lista);
+        //JRDataSource beanDataSource = new JRBeanCollectionDataSource(lista);
         String reportPath = FacesContext.getCurrentInstance().getExternalContext().getRealPath("reportes/report3.jasper");
-
-         jasperPrint = JasperFillManager.fillReport( reportPath ,new HashMap() , beanDataSource );
-        
-        //JRBeanCollectionDataSource beanCollectionDataSource = new JRBeanCollectionDataSource(lista, false);
-        //jasperPrint = JasperFillManager.fillReport(reportPath, new HashMap(), beanCollectionDataSource);
+//        jasperPrint = JasperFillManager.fillReport( reportPath ,new HashMap() , beanDataSource );
+        JRBeanCollectionDataSource beanCollectionDataSource = new JRBeanCollectionDataSource(lista, false);
+        jasperPrint = JasperFillManager.fillReport(reportPath, new HashMap(), beanCollectionDataSource);
     }
 
     public void PDF() throws JRException, IOException {
@@ -62,6 +65,48 @@ public class UsuarioBean implements Serializable {
         ServletOutputStream servletOutputStream = httpServletResponse.getOutputStream();
         JasperExportManager.exportReportToPdfStream(jasperPrint, servletOutputStream);
         FacesContext.getCurrentInstance().responseComplete();
+    }
+    public void EXCEL() throws JRException, IOException {
+        init();
+        HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+        httpServletResponse.addHeader("Content-disposition", "attachment; filename=report4.xls");
+        ServletOutputStream servletOutputStream = httpServletResponse.getOutputStream();
+        JRXlsxExporter exporter=new JRXlsxExporter();
+        exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+        exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, servletOutputStream);
+        exporter.exportReport();
+        servletOutputStream.flush();
+        servletOutputStream.close();
+        FacesContext.getCurrentInstance().responseComplete();
+        
+    }
+    public void PPT() throws JRException, IOException {
+        init();
+        HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+        httpServletResponse.addHeader("Content-disposition", "attachment; filename=report4.ppt");
+        ServletOutputStream servletOutputStream = httpServletResponse.getOutputStream();
+        JRPptxExporter exporter=new JRPptxExporter();
+        exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+        exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, servletOutputStream);
+        exporter.exportReport();
+        servletOutputStream.flush();
+        servletOutputStream.close();
+        FacesContext.getCurrentInstance().responseComplete();
+        
+    }
+     public void DOC() throws JRException, IOException {
+        init();
+        HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+        httpServletResponse.addHeader("Content-disposition", "attachment; filename=report4.doc");
+        ServletOutputStream servletOutputStream = httpServletResponse.getOutputStream();
+        JRDocxExporter exporter=new JRDocxExporter ();
+        exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+        exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, servletOutputStream);
+        exporter.exportReport();
+        servletOutputStream.flush();
+        servletOutputStream.close();
+        FacesContext.getCurrentInstance().responseComplete();
+        
     }
 
 }
